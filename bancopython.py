e1eefdf9b37e0972f40386dbf1dc04d692f2da4e
Lucas de Oliveira <91337785+olucasoliveira@users.noreply.github.com>
2022-05-14 16:43:55 -0300

    Update bancopython.py

--- bancopython.py
+++ bancopython.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-con = mysql.connector.connect(host='localhost',  database='cadastro', user= 'root', password='lucas2003@')
+con = mysql.connector.connect(host='localhost',  database='cadastro', user= 'root', password='*******')
 
 if con.is_connected():
     db_info = con.get_server_info()
